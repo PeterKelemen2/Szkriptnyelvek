a3a50dff08f192102c9951789dababc54fdc7ec3
PeterKelemen2 <90985214+PeterKelemen2@users.noreply.github.com>
2023-11-28 19:15:10 +0100

    Add files via upload

---
+++ gyakorlas/feladat2/feladat2.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def read_strings():
+    end_word = input("End word:")
+    while True:
+        current_word = input("Szo: ")
+        if current_word == end_word:
+            print("END")
+            break
+        else:
+            print(get_string_info(current_word))
+
+
+def get_string_info(word):
+    info = {"upper": 0,
+            "lower": 0,
+            "length": len(word)}
+    for c in word:
+        if c == c.upper():
+            info["upper"] += 1
+        elif c == c.lower():
+            info["lower"] += 1
+
+    return info
+
+
+def main():
+    read_strings()
+
+
+if __name__ == "__main__":
+    main()
